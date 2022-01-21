90fedaf5cd9a0f300b7c6ff94478929b89df8e5b
Dylan Jacob <business.dmjacob@gmail.com>
2022-01-21 09:19:42 -0500

    testing out some things

--- app.js
+++ app.js
@@ -1,13 +1,59 @@
 const loadingBarItemEl = document.querySelector(".loading-bar__item");
+const percentageButtonsEl = document.getElementsByClassName("percentage__buttons");
 
 let width = 1;
-let loadingBar = setInterval(function() {
-    if (width >= 100) {
-        clearInterval(loadingBar);
-    } else {
-        width++;
-        loadingBarItemEl.style.width = width + "%";
-    }
-}, 15)
+// let loadingBar = setInterval(function() {
+//     if (width >= 100) {
+//         clearInterval(loadingBar);
+//     } else {
+//         width++;
+//         loadingBarItemEl.style.width = width + "%";
+//     }
+// }, 15);
 
-// TODO: 30%, 60%, 100%
+function buttonPress() {
+    for (let item of percentageButtonsEl) {
+        let buttonThrityEl = item.firstElementChild;
+        buttonThrityEl.addEventListener("click", function() {
+            let thirtyLoadingBar = setInterval(function() {
+                // window.location.reload();
+                if (width >= 30) {
+                    clearInterval(thirtyLoadingBar);
+                } else {
+                    width++;
+                    loadingBarItemEl.style.width = width + "%";
+                }
+            }, 15);
+        });
+        let buttonSixtyEl= buttonThrityEl.nextElementSibling;
+        buttonSixtyEl.addEventListener("click", function() {
+            let sixtyLoadingBar = setInterval(function() {
+                // window.location.reload();
+                if (width >= 60) {
+                    clearInterval(sixtyLoadingBar);
+                } else {
+                    width++;
+                    loadingBarItemEl.style.width = width + "%"
+                }
+            },15 );
+        });
+        let buttonOneHundredEl = buttonSixtyEl.nextElementSibling;
+        buttonOneHundredEl.addEventListener("click", function() {
+            let oneHundredLoadingBar = setInterval(function() {
+                // window.location.reload();
+                if (width >= 100) {
+                    clearInterval(oneHundredLoadingBar);
+                } else {
+                    width++;
+                    loadingBarItemEl.style.width = width + "%"
+                }
+            }, 15)
+        })
+    }
+}
+
+
+
+buttonPress();
+
+// TODO: figure out a way to condense the repeated code
